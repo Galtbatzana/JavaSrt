4669f768498b5408b311913181ecb6859a02036b
Galtbatzana <164704929+Galtbatzana@users.noreply.github.com>
2024-05-27 19:10:21 +0800

    array index

---
+++ 20240527.js
@@ -0,0 +1,19 @@
+
+
+
+const mgl = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'ө', 'п', 'р', 'с', 'т', 'у', 'ү', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ь', 'э', 'ю', 'я'];
+
+// for (let i = 0; i < mgl.length; i++) {
+//     let useg = i + 1;
+//     console.log(mgl[i], useg)
+// }
+
+function myFunction1(num) {
+    
+    let result = mgl[i] == num;
+
+    return result;
+
+}
+
+console.log(myFunction1(14))
---
+++ 240522-hicheel.js
@@ -0,0 +1,102 @@
+
+// const array = [
+//     [7, 14, 34],
+//     [2, 21, 20],
+//     [13, 12, 36],
+// ];
+
+// for (let i = 0; i < array.length; i++) {
+//     let sum = 0;
+//     for (let j = 0; j < array.length; j++) {
+//         sum += array[i][j];
+//     }
+//     console.log(sum);
+// }
+
+// const array = [6, 2, 3, 1, 5, 10]
+// let num = 0;
+
+// for (let i = 0; i < array; i++) {
+//     console.log(array);
+// }
+
+// const nums = [1, 1, 2]; // [1, "", 2]
+// const nums2 = [1, 1, 1, 2, 2, 3]; // [1,"", "", 2, "" ,3]
+
+
+// for (let i = 1; i < nums.length; i++) {
+//     if (nums[i] === 1 || nums[i] === 2) {
+//         nums[i] = "_";
+
+//     } console.log(nums)
+
+// }
+
+// for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//         if (nums[i] === nums[j]) {
+//             nums[j] = "_";
+//         }
+//     } console.log(nums);
+
+// }
+
+
+// const nums = [1, 1, 2]; // [1, "", 2]
+// for (let i = 1; i < nums.length; i++) {
+//     if (nums[i] == nums[i - 1]) {
+//         nums[i] = "_";
+
+//     } console.log(nums)
+
+// }
+
+
+
+// // 1.
+    // nums1 = [1, 2, 2, 1], nums2 = [2, 2] // output [2,2]
+    /*
+nums1 = [4, 9, 5], nums2 = [9, 4, 9, 8, 4] // output [4,9]
+
+    let result = []
+    for (let i = 0; i < nums1.length; i++) {
+        
+        for (let j = 0; j < nums2.length; j++) {
+            
+        if (nums1[i] === nums2[j]) {
+            result.push(nums2[j]);
+            break
+            }
+
+        }
+
+    }
+    console.log(result)
+    */
+
+
+
+
+// 2.
+// nums1 = [1,2,3], nums2 = [2,4,6] // output  [[1,3],[4,6]]
+// nums1 = [1,2,3,3], nums2 = [1,1,2,2] // [[3],[]]
+// array dotor davhatssan toog arilgah
+
+
+// const duplicate = [1, 1, 1, 2, 2]; //[1,2]
+
+// let num = [];
+// for (let i = 0; i < duplicate.length; i++) {
+//     // console.log(i, duplicate[i])
+//     if (duplicate[i] !== duplicate[i - 1]) {
+//         // console.log(i, duplicate[i])
+//         num.push(duplicate[i]);
+//     }
+
+// }
+// console.log(num)
+
+
+// sort agaas ih ruu
+//const nums = [3, 4, 1, 1, 1, 1, 2]; //[1, 2, 3]
+
