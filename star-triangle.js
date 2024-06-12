7c8521dce5b9e1e6f345ee200351c66c388a1563
Galtbatzana <164704929+Galtbatzana@users.noreply.github.com>
2024-06-13 02:25:38 +0800

    nested loop

--- star-triangle.js
+++ star-triangle.js
@@ -1,46 +1,35 @@
-function solution(n) {
+function run() {
+    const n = getNumberValueFromInput();
+    const output = multiTable(n);
+    print(output);
+}
+
+
+function multiTable(n) {
     let result = "";
 
-    for (let i = 0; i <=n; i++) {
-       // generate spaces here... // 
-      
-       result = result + generateSpace(i);
-       result = result + "\n";  
+    for (let i = 1; i <= n; i++) {
+        result = result + generateStars(i);
+        result = result + "\n";
     }
     return result;
 }
-//////////////////////////////////////////////////////
-    // od hevleh 
+
+
 function generateStars(numberOfStars) {
     let stars = "";
-    
-    for (let i = 0; i < numberOfStars; i++) {   
-        stars = stars + "* "; 
-    } return stars;
+    for (let i = 0; i < numberOfStars; i++) {
+        stars = stars + "* ";  
+    }
+    return stars;
 }
 
-
-//////////////////////////////////////////////////////
-function generateSpace(numberOfSpace) {
-    let space = " ";
-    for (let i = numberOfSpace; i > -1; i--) {
-       space = space + " *"
-       
-    } return space;
+function getNumberValueFromInput() {
+    const inputString = document.getElementById("input").value;
+    const n = Number(inputString);
+    return n;
 }
 
-////////////////////////////////////////////////////
-// function starTriangle (n) {
-//     let result = "";
-
-//     for (let i = 0; i < n; i++) {
-//         result = result + generateStars(i)
-//         result
-    
-//     } return result;
-// }
-
-
-
-
-
+function print(output) {
+    document.getElementById("output").innerText = output;
+}
