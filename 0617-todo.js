ed791004e06b7426718fb188d8c91168215a31c4
Galtbatzana <164704929+Galtbatzana@users.noreply.github.com>
2024-06-18 05:12:41 +0800

    updated home

--- 0617-todo.js
+++ 0617-todo.js
@@ -1,10 +1,17 @@
 
-
-
 function addTask() {
     const input = document.getElementById("input");
     console.log(input.value);
-    constent.innerHTML = content.innerHTML + "<div>" + input.value + "</div>";
+    // constent.innerHTML = content.innerHTML + "<div>" + input.value + "</div>";   
+}
+addTask();
+
+
+const todoText = document.getElementsById("title");
+console.log(todoText.innerText);
+
+function editText() {
+    todoText = "";
+    const todoText = document.getElementsById("title");
     
 }
-
